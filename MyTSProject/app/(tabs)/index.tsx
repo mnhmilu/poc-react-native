f058e2bed79af7c2ed4f1739fcf119b47e2bba33
mnhmilu <mnh_milu@yahoo.com>
2023-06-20 17:05:40 +0600

    CRUD Operation shifted to Model

--- MyTSProject/app/(tabs)/index.tsx
+++ MyTSProject/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+
 import {
   ScrollView,
   View,
@@ -9,95 +9,11 @@ import {
 } from "react-native";
 import { PricingCard, lightColors } from "@rneui/themed";
 import { useRouter } from "expo-router";
-//import { openDatabase, insertData } from '../../database';
-import * as SQLite from "expo-sqlite";
 
 type PricingCardComponentProps = {};
 
 const Pricing: React.FunctionComponent<PricingCardComponentProps> = () => {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
-  const [names, setNames] = useState([]);
-  const db = SQLite.openDatabase("test.db");
-  const [currentName, setCurrentName] = useState(undefined);
-
-  useEffect(() => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        "CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);"
-      );
-
-      tx.executeSql("select * from names", [], (_, { rows }) =>
-        console.log(JSON.stringify(rows))
-      );
-      tx.executeSql(
-        "SELECT * FROM names",
-        null,
-        (txObj, resultSet) => setNames(resultSet.rows._array),
-        (txObj, error) => console.log(error)
-      );
-    });
-  }, []);
-
-  const addName = () => {
-    console.log('inserting data------------------_>')
-    db.transaction((tx) => {
-      tx.executeSql(
-        "INSERT INTO names (name) values (?)",
-        [currentName],
-        (txObj, resultSet) => {
-          let existingNames = [...names];
-          existingNames.push({ id: resultSet.insertId, name: currentName });
-          setNames(existingNames);
-          setCurrentName(undefined);
-        },
-        (txObj, error) => console.log(error)
-      );
-    });
-  };
-
-  const deleteName = (id) => {
-    db.transaction(tx => {
-      tx.executeSql('DELETE FROM names WHERE id = ?', [id],
-        (txObj, resultSet) => {
-          if (resultSet.rowsAffected > 0) {
-            let existingNames = [...names].filter(name => name.id !== id);
-            setNames(existingNames);
-          }
-        },
-        (txObj, error) => console.log(error)
-      );
-    });
-  };
-
-  const updateName = (id) => {
-    db.transaction(tx => {
-      tx.executeSql('UPDATE names SET name = ? WHERE id = ?', [currentName, id],
-        (txObj, resultSet) => {
-          if (resultSet.rowsAffected > 0) {
-            let existingNames = [...names];
-            const indexToUpdate = existingNames.findIndex(name => name.id === id);
-            existingNames[indexToUpdate].name = currentName;
-            setNames(existingNames);
-            setCurrentName(undefined);
-          }
-        },
-        (txObj, error) => console.log(error)
-      );
-    });
-  };
-
-  const showNames = () => {
-    return names.map((name, index) => {
-      return (
-        <View key={index} style={styles.row}>
-          <Text>{name.name}</Text>
-          <Button title="Delete" onPress={() => deleteName(name.id)} />
-          <Button title="Update" onPress={() => updateName(name.id)} />
-        </View>
-      );
-    });
-  };
 
   return (
     <>
@@ -112,13 +28,7 @@ const Pricing: React.FunctionComponent<PricingCardComponentProps> = () => {
           info={["Total Accouns 112", "Basic Support", "All Core Features"]}
           button={{ title: "Add Transaction", icon: "add" }}
         />
-        <TextInput
-          value={currentName}
-          placeholder="name"
-          onChangeText={setCurrentName}
-        />
-        <Button title="Add Name" onPress={addName} />
-        {showNames()}
+ 
       </ScrollView>
     </>
   );
--- MyTSProject/app/modal.tsx
+++ MyTSProject/app/modal.tsx
@@ -1,35 +1,145 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Button, TextInput } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import * as SQLite from "expo-sqlite";
 
 export default function ModalScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>New Transaction</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
+  const [isLoading, setIsLoading] = useState(true);
+  const [names, setNames] = useState([]);
+  const db = SQLite.openDatabase("test.db");
+  const [currentName, setCurrentName] = useState(undefined);
 
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-    </View>
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);"
+      );
+
+      tx.executeSql("select * from names", [], (_, { rows }) =>
+        console.log(JSON.stringify(rows))
+      );
+      tx.executeSql(
+        "SELECT * FROM names",
+        null,
+        (txObj, resultSet) => setNames(resultSet.rows._array),
+        (txObj, error) => console.log(error)
+      );
+    });
+  }, []);
+
+  const addName = () => {
+    console.log("inserting data------------------_>");
+    db.transaction((tx) => {
+      tx.executeSql(
+        "INSERT INTO names (name) values (?)",
+        [currentName],
+        (txObj, resultSet) => {
+          let existingNames = [...names];
+          existingNames.push({ id: resultSet.insertId, name: currentName });
+          setNames(existingNames);
+          setCurrentName(undefined);
+        },
+        (txObj, error) => console.log(error)
+      );
+    });
+  };
+
+  const deleteName = (id) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "DELETE FROM names WHERE id = ?",
+        [id],
+        (txObj, resultSet) => {
+          if (resultSet.rowsAffected > 0) {
+            let existingNames = [...names].filter((name) => name.id !== id);
+            setNames(existingNames);
+          }
+        },
+        (txObj, error) => console.log(error)
+      );
+    });
+  };
+
+  const updateName = (id) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "UPDATE names SET name = ? WHERE id = ?",
+        [currentName, id],
+        (txObj, resultSet) => {
+          if (resultSet.rowsAffected > 0) {
+            let existingNames = [...names];
+            const indexToUpdate = existingNames.findIndex(
+              (name) => name.id === id
+            );
+            existingNames[indexToUpdate].name = currentName;
+            setNames(existingNames);
+            setCurrentName(undefined);
+          }
+        },
+        (txObj, error) => console.log(error)
+      );
+    });
+  };
+
+  const showNames = () => {
+    return names.map((name, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          <Text>{name.name}</Text>
+          <Button title="Delete" onPress={() => deleteName(name.id)} />
+          <Button title="Update" onPress={() => updateName(name.id)} />
+        </View>
+      );
+    });
+  };
+
+  return (
+    <>
+      <ScrollView>
+        <TextInput
+          value={currentName}
+          placeholder="name"
+          onChangeText={setCurrentName}
+        />
+        <Button title="Add Name" onPress={addName} />
+        {showNames()}
+      </ScrollView>
+    </>
   );
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: "80%",
+//   },
+// });
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "stretch",
+    justifyContent: "space-between",
+    margin: 8,
   },
 });
